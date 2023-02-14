60e40d4c3be620db6024e030593ab655b3ab1948
gabgaleski <galeski35@gmail.com>
2023-02-14 15:03:20 -0300

    Realiza requisito 1

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,17 +5,23 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList).toBe('function');
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalled();
 
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
 
   });
 
-  // it('...', () => {
-  // });
+  it('Ao chamar a função fetchProductsList sem argumento, retorna um erro', async () => {
+    const undefined = await fetchProductsList();
+    expect(undefined).rejects.toThrow('Termo de busca não informado');
+  }) 
 });
