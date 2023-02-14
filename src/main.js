7a9eb26f101f9298a1848beb7e860a7f342620fd
gabgaleski <galeski35@gmail.com>
2023-02-14 16:32:41 -0300

    Realiza requisito 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
+const products = document.querySelector('.products');
+
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+function listGenerate(component) {
+  const array = fetchProductsList(component);
+  array.then((data) => data.forEach((element) => {
+    const { id, title, thumbnail, price } = element;
+    const newProduct = createProductElement({ id, title, thumbnail, price });
+    products.appendChild(newProduct);
+  }));
+}
+listGenerate('computador');
