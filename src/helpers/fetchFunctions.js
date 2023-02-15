a5543eec1e19282a0d9169b982cd56c66d433c92
gabgaleski <galeski35@gmail.com>
2023-02-15 15:56:06 -0300

    Realiza requisito 6

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -6,12 +6,8 @@ export const fetchProductsList = async (param) => {
   if (param === undefined) {
     throw new Error('Termo de busca não informado');
   }
-  try {
-    const API = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${param}`);
-    const formation = await API.json();
-    const result = await formation.results;
-    return result;
-  } catch (error) {
-    return error.message;
-  }
+  const API = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${param}`);
+  const formation = await API.json();
+  const result = await formation.results;
+  return result;
 };
