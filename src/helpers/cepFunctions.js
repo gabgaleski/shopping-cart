fefcc05aef98ce46b2f4ea0b518a23ec8dd978c1
gabgaleski <galeski35@gmail.com>
2023-02-16 14:29:16 -0300

    Realiza requisito 11

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,25 +1,24 @@
 export const getAddress = async () => {
   const getCep = document.querySelector('.cep-input');
   const cep = getCep.value;
-  const getSpan = document.querySelector('.cart__address');
   try {
     const firstAPI = (`https://cep.awesomeapi.com.br/json/${cep}`);
     const secondAPI = (`https://brasilapi.com.br/api/cep/v2/${cep}`);
     const promises = await Promise.any([fetch(firstAPI), fetch(secondAPI)]);
-    if (!promises.ok) {
-      getSpan.innerHTML = 'CEP não encontrado';
-      return;
-    }
-    const result = promises.json();
+    const result = await promises.json();
     return result;
-  } catch (error) {
-    getSpan.innerHTML = 'CEP não encontrado';
+  } catch {
+    return 'CEP não encontrado';
   }
 };
 
 export const searchCep = async () => {
   const getSpan = document.querySelector('.cart__address');
   const data = await getAddress();
+  if (!data.city) {
+    getSpan.innerHTML = 'CEP não encontrado';
+    return;
+  }
   const { address, district, city, state, street, neighborhood } = data;
   if (address) {
     getSpan.innerHTML = `${address} - ${district} - ${city} - ${state}`;
